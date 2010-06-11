9b8783e208e06d26376f4bf5ead841cf22380333
Javier Torrente Vigil <jtorrente84@gmail.com>
2010-06-11 01:16:15 +0000

    Copyright Header Updated to LGPL v3

--- E-Adventure/src/es/eucm/eadventure/editor/control/security/jarsigner/JSSigner.java
+++ E-Adventure/src/es/eucm/eadventure/editor/control/security/jarsigner/JSSigner.java
@@ -1,20 +1,39 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with this
- * work for additional information regarding copyright ownership. The ASF
- * licenses this file to You under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+/*******************************************************************************
+ * <e-Adventure> (formerly <e-Game>) is a research project of the <e-UCM>
+ *         research group.
+ *  
+ *   Copyright 2005-2010 <e-UCM> research group.
  * 
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   You can access a list of all the contributors to <e-Adventure> at:
+ *         http://e-adventure.e-ucm.es/contributors
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
+ *   <e-UCM> is a research group of the Department of Software Engineering
+ *         and Artificial Intelligence at the Complutense University of Madrid
+ *         (School of Computer Science).
+ * 
+ *         C Profesor Jose Garcia Santesmases sn,
+ *         28040 Madrid (Madrid), Spain.
+ * 
+ *         For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *         <http://www.e-ucm.es>
+ * 
+ * ****************************************************************************
+ * 
+ * This file is part of <e-Adventure>, version 1.2.
+ * 
+ *     <e-Adventure> is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     <e-Adventure> is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with <e-Adventure>.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package es.eucm.eadventure.editor.control.security.jarsigner;
 
 import java.io.ByteArrayInputStream;
--- E-Adventure/src/es/eucm/eadventure/editor/gui/elementpanels/assessment/AssessmentEditionPanel.java
+++ E-Adventure/src/es/eucm/eadventure/editor/gui/elementpanels/assessment/AssessmentEditionPanel.java
@@ -1,36 +1,39 @@
-/**
- * <e-Adventure> is an <e-UCM> research project. <e-UCM>, Department of Software
- * Engineering and Artificial Intelligence. Faculty of Informatics, Complutense
- * University of Madrid (Spain).
+/*******************************************************************************
+ * <e-Adventure> (formerly <e-Game>) is a research project of the <e-UCM>
+ *         research group.
+ *  
+ *   Copyright 2005-2010 <e-UCM> research group.
  * 
- * @author Del Blanco, A., Marchiori, E., Torrente, F.J. (alphabetical order) *
- * @author López Mañas, E., Pérez Padilla, F., Sollet, E., Torijano, B. (former
- *         developers by alphabetical order)
- * @author Moreno-Ger, P. & Fernández-Manjón, B. (directors)
- * @year 2009 Web-site: http://e-adventure.e-ucm.es
- */
-
-/*
- * Copyright (C) 2004-2009 <e-UCM> research group
+ *   You can access a list of all the contributors to <e-Adventure> at:
+ *         http://e-adventure.e-ucm.es/contributors
  * 
- * This file is part of <e-Adventure> project, an educational game & game-like
- * simulation authoring tool, available at http://e-adventure.e-ucm.es.
+ *   <e-UCM> is a research group of the Department of Software Engineering
+ *         and Artificial Intelligence at the Complutense University of Madrid
+ *         (School of Computer Science).
  * 
- * <e-Adventure> is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option) any
- * later version.
+ *         C Profesor Jose Garcia Santesmases sn,
+ *         28040 Madrid (Madrid), Spain.
  * 
- * <e-Adventure> is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
+ *         For more info please visit:  <http://e-adventure.e-ucm.es> or
+ *         <http://www.e-ucm.es>
  * 
- * You should have received a copy of the GNU General Public License along with
- * <e-Adventure>; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
+ * ****************************************************************************
  * 
- */
+ * This file is part of <e-Adventure>, version 1.2.
+ * 
+ *     <e-Adventure> is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     <e-Adventure> is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with <e-Adventure>.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package es.eucm.eadventure.editor.gui.elementpanels.assessment;
 
 import java.awt.BorderLayout;
