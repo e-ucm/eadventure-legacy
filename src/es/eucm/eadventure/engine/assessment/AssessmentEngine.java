6ef1da1b3c46699ce4bce1fb4a937f7207ef24a2
Javier Torrente Vigil <jtorrente84@gmail.com>
2010-11-04 15:50:34 +0000

    Minor improvements in assessment engine: generation of HTML reports. Now the report is stored in disk only if necessary (e.g. to be sent through email)

--- src/es/eucm/eadventure/engine/assessment/AssessmentEngine.java
+++ src/es/eucm/eadventure/engine/assessment/AssessmentEngine.java
@@ -48,7 +48,6 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.io.StringWriter;
-import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -79,7 +78,6 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import es.eucm.eadventure.comm.manager.commManager.CommManagerLAMS;
-import es.eucm.eadventure.common.auxiliar.ReleaseFolders;
 import es.eucm.eadventure.common.auxiliar.SendMail;
 import es.eucm.eadventure.common.data.assessment.AssessmentProfile;
 import es.eucm.eadventure.common.data.assessment.AssessmentProperty;
@@ -379,7 +377,27 @@ public class AssessmentEngine implements TimerEventListener {
 	}
 
 	//TODO nowadays it is only used by LAMS!!
-	public String generateReportToSend(){
+	public String getHTMLReportStringLAMS(){
+	    CommManagerLAMS comm = ((CommManagerLAMS)Game.getInstance( ).getComm( ));
+	    return getHTMLReportString(": " + comm.getUserFName( ) + " " + comm.getUserLName( ) , false, 0);
+	}
+
+	/**
+	 * Returns the HTML code of the assessment report
+	 * @return
+	 */
+   public String getHTMLReportString(){
+        return getHTMLReportString("", true, -5);
+    }
+	
+   /**
+    * Returns a string with the HTML code of the assessment report
+    * @param textToAppendInTitle This text will be appended to the title of the report
+    * @param styleBackgroundTag If true, the background tag will be styled using css. Otherwise just "<body>" will be used
+    * @param minImportance All the rules less important than this threshold will not be appended to the report
+    * @return The HTML code
+    */
+	public String getHTMLReportString(String textToAppendInTitle, boolean styleBackgroundTag, int minImportance){
 	String file = new String();
 	    
 	    // HTML tag
@@ -391,7 +409,11 @@ public class AssessmentEngine implements TimerEventListener {
         file+="</title>";
 
         // Body and content table
-        file+="<body>";
+        if (!styleBackgroundTag)
+            file+="<body>";
+        else
+            file+="<body style=\"background: #"+HTML_REPORT_COLOR_0+";\">";
+        
         file+="<br/><br/>";
         file+="<table width=\"80%\" align=\"center\" style=\"background : #"+HTML_REPORT_COLOR_1+"; border : 1px solid #000000;\">";
         file+="<tr><td>";
@@ -399,8 +421,7 @@ public class AssessmentEngine implements TimerEventListener {
         // Title
         file+="<center><h1>";
         file+=Game.getInstance( ).processText(Game.getInstance().getGameDescriptor().getTitle());
-        CommManagerLAMS comm = ((CommManagerLAMS)Game.getInstance( ).getComm( ));
-        file+=" report: " + comm.getUserFName( ) + " " + comm.getUserLName( ) ;
+        file+=" report"+textToAppendInTitle;
         file+="</h1></center>";
 
         // Clear table
@@ -411,8 +432,8 @@ public class AssessmentEngine implements TimerEventListener {
         // For each processed rule
         for (ProcessedRule rule : processedRules) {
             // First check the importance
-            if (rule.getImportance() >= 0) {
-                file+=rule.getHTMLCode();
+            if (rule.getImportance() >= minImportance) {
+                file+=Game.getInstance( ).processText(rule.getHTMLCode());
                 file+="<br/><br/>";
             }
         }
@@ -441,59 +462,12 @@ public class AssessmentEngine implements TimerEventListener {
 	 * @param minImportance
 	 *            Importance value for filtering
 	 */
-	public void generateHTMLReport(String filename, int minImportance) {
+	public void generateHTMLReportFile(String filename, int minImportance) {
 		try {
 			// Create the file to write
 			PrintStream file = new PrintStream(new FileOutputStream(filename));
 
-			// HTML tag
-			file.println("<html>");
-
-			// Header
-			file.print("<title>");
-			file.print(Game.getInstance( ).processText( Game.getInstance().getGameDescriptor().getTitle()) );
-			file.println("</title>");
-
-			// Body and content table
-			file.println("<body style=\"background: #"+HTML_REPORT_COLOR_0+";\">");
-			file.println("<br/><br/>");
-			file
-					.println("<table width=\"80%\" align=\"center\" style=\"background : #"+HTML_REPORT_COLOR_1+"; border : 1px solid #000000;\">");
-			file.println("<tr><td>");
-
-			// Title
-			file.print("<center><h1>");
-			file.print(Game.getInstance( ).processText(Game.getInstance().getGameDescriptor().getTitle()));
-			file.print(" report");
-			file.println("</h1></center>");
-
-			// Clear table
-			file.println("<br/><br/>");
-			file
-					.println("<table width=\"95%\" align=\"center\" style=\"background : #"+HTML_REPORT_COLOR_2+"; border : 1px solid #000000\">");
-			file.println("<tr><td>");
-
-			// For each processed rule
-			for (ProcessedRule rule : processedRules) {
-				// First check the importance
-				if (rule.getImportance() >= minImportance) {
-					file.println(Game.getInstance( ).processText(rule.getHTMLCode()));
-					file.println("<br/><br/>");
-				}
-			}
-
-			// Close clear table
-			file.println("</td></tr>");
-			file.println("</table>");
-
-			// Close table and body
-			file.println("<br/><br/>");
-			file.println("</td></tr>");
-			file.println("</table>");
-			file.println("</body>");
-
-			// Close HTML
-			file.println("</html>");
+			file.print( getHTMLReportString( ) );
 
 			// Close the file
 			file.close();
@@ -588,147 +562,133 @@ public class AssessmentEngine implements TimerEventListener {
 		// )) {
 		if (assessmentProfile!=null){
 		if (state == STATE_NONE && assessmentProfile.isShowReportAtEnd()) {
-			try {
-				state = STATE_STARTED;
-				int i = 0;
-				File reportFile = null;
-				String fileName = null;
-				do {
-					i++;
-					fileName = "Report_" + i + ".html";
-					reportFile = new File(ReleaseFolders.reportsFolder(), fileName);
-				} while (reportFile.exists());
-				if (!ReleaseFolders.reportsFolder().exists()) {
-					ReleaseFolders.reportsFolder().mkdirs();
-				}
-				reportFile.createNewFile();
-				final String reportAbsoluteFile = reportFile.getAbsolutePath();
-				generateHTMLReport(reportFile.getAbsolutePath(), -5);
+			state = STATE_STARTED;
 
-				JEditorPane reportPanel = new JEditorPane();
-				reportPanel.setContentType("text/html");
+            JEditorPane reportPanel = new JEditorPane();
+            reportPanel.setContentType("text/html");
 
-				reportPanel.setPage(reportFile.toURI().toURL());
-				reportPanel.setEditable(false);
+            reportPanel.setText( getHTMLReportString(  ) );
+            reportPanel.setEditable(false);
 
-				JScrollPane contentPanel = new JScrollPane(reportPanel,
-						JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-						JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-				JPanel panel = new JPanel();
-				panel.setLayout(new BorderLayout());
-				panel.add(contentPanel, BorderLayout.CENTER);
+            JScrollPane contentPanel = new JScrollPane(reportPanel,
+            		JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+            		JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+            JPanel panel = new JPanel();
+            panel.setLayout(new BorderLayout());
+            panel.add(contentPanel, BorderLayout.CENTER);
 
-				JPanel buttonPanel = new JPanel();
-				if (!assessmentProfile.isSendByEmail()) {
-					JButton ok = new JButton("OK");
-					ok.addActionListener(new ActionListener() {
+            JPanel buttonPanel = new JPanel();
+            if (!assessmentProfile.isSendByEmail()) {
+            	JButton ok = new JButton("OK");
+            	ok.addActionListener(new ActionListener() {
 
-						public void actionPerformed(ActionEvent e) {
-							GUI.getInstance().restoreFrame();
-							state = STATE_DONE;
-						}
+            		public void actionPerformed(ActionEvent e) {
+            			GUI.getInstance().restoreFrame();
+            			state = STATE_DONE;
+            		}
 
-					});
-					buttonPanel.add(ok);
-				} else {
-					final JButton ok_send = new JButton(TC
-							.get("Report.OKSend"));
-					if (playerName == null || playerName.equals("")) {
-					    ok_send.setEnabled( false );
-					    JLabel label =new JLabel(TC.get("Report.Name"));
-					    label.setForeground( Color.white );
-					    buttonPanel.add(label);
-					    JTextField nameTextField = new JTextField(30);
-					    nameTextField.getDocument( ).addDocumentListener( new DocumentListener() {
-                            public void changedUpdate( DocumentEvent e ) {
-                                update(e);
+            	});
+            	buttonPanel.add(ok);
+            } else {
+            	final JButton ok_send = new JButton(TC
+            			.get("Report.OKSend"));
+            	if (playerName == null || playerName.equals("")) {
+            	    ok_send.setEnabled( false );
+            	    JLabel label =new JLabel(TC.get("Report.Name"));
+            	    label.setForeground( Color.white );
+            	    buttonPanel.add(label);
+            	    JTextField nameTextField = new JTextField(30);
+            	    nameTextField.getDocument( ).addDocumentListener( new DocumentListener() {
+                        public void changedUpdate( DocumentEvent e ) {
+                            update(e);
+                        }
+                        public void insertUpdate( DocumentEvent e ) {
+                            update(e);
+                        }
+                        public void removeUpdate( DocumentEvent e ) {
+                            update(e);
+                        }
+                        private void update(DocumentEvent e) {
+                            try {
+                                playerName = e.getDocument( ).getText( 0, e.getDocument( ).getLength( ));
                             }
-                            public void insertUpdate( DocumentEvent e ) {
-                                update(e);
-                            }
-                            public void removeUpdate( DocumentEvent e ) {
-                                update(e);
-                            }
-                            private void update(DocumentEvent e) {
-                                try {
-                                    playerName = e.getDocument( ).getText( 0, e.getDocument( ).getLength( ));
-                                }
-                                catch( BadLocationException e1 ) {
-                                    e1.printStackTrace();
-                                }
-                                if (playerName == null || playerName.equals(""))
-                                    ok_send.setEnabled( false );
-                                else
-                                    ok_send.setEnabled( true );
-                            }
-					    });
-					    buttonPanel.add(nameTextField);
-					}
-					
-					
-					ok_send.addActionListener(new ActionListener() {
-						public void actionPerformed(ActionEvent e) {
-							String[] to = new String[1];
-							to[0] = assessmentProfile.getEmail();
-							String subject = "Report eAdventure";
-							String message = "Report from: " + playerName;
-							
-							//TODO: eliminar esto, se a–adi— para las pruebas en medicina
-							/*
-							File report = new File(reportAbsoluteFile);
-							try {
-							    FileReader fir = new FileReader(report);
-	                            BufferedReader br = new BufferedReader(fir);
-	                            String line = br.readLine( );
-	                            String text = "";
-	                            while (line != null) {
-	                                text += line + "\n\r";
-	                                line = br.readLine( );
-	                            }
-	                            es.eucm.eadventure.common.auxiliar.ReportDialog.sendReport(subject + "\n\r" + message + "\n\r" + text);
-                            }
-                            catch( FileNotFoundException e1 ) {
+                            catch( BadLocationException e1 ) {
                                 e1.printStackTrace();
                             }
-                            catch( IOException e1 ) {
-                                e1.printStackTrace();
+                            if (playerName == null || playerName.equals(""))
+                                ok_send.setEnabled( false );
+                            else
+                                ok_send.setEnabled( true );
+                        }
+            	    });
+            	    buttonPanel.add(nameTextField);
+            	}
+            	
+            	
+            	ok_send.addActionListener(new ActionListener() {
+            		public void actionPerformed(ActionEvent e) {
+            			String[] to = new String[1];
+            			to[0] = assessmentProfile.getEmail();
+            			String subject = "Report eAdventure";
+            			String message = "Report from: " + playerName;
+            			
+            			//TODO: eliminar esto, se a–adi— para las pruebas en medicina
+            			/*
+            			File report = new File(reportAbsoluteFile);
+            			try {
+            			    FileReader fir = new FileReader(report);
+                            BufferedReader br = new BufferedReader(fir);
+                            String line = br.readLine( );
+                            String text = "";
+                            while (line != null) {
+                                text += line + "\n\r";
+                                line = br.readLine( );
                             }
-							*/
-							
-							SendMail sm = new SendMail(assessmentProfile.getSmtpServer(), assessmentProfile.getSmtpUser(), assessmentProfile.getSmtpPwd());
-							sm.setPort(Integer.parseInt(assessmentProfile.getSmtpPort()));
-							sm.setRequiersSSL(assessmentProfile.isSmtpSSL());
-							sm.postMailAttachements(to, subject, message, null,reportAbsoluteFile);
-							GUI.getInstance().restoreFrame();
-							state = STATE_DONE;
-						}
-					});
-					buttonPanel.add(ok_send);
-					JButton ok_dont_send = new JButton(TC
-							.get("Report.OKDontSend"));
-					ok_dont_send.addActionListener(new ActionListener() {
-						public void actionPerformed(ActionEvent e) {
-							GUI.getInstance().restoreFrame();
-							state = STATE_DONE;
-						}
-					});
-					buttonPanel.add(ok_dont_send);
-					// buttonPanel.getRootPane().setDefaultButton(ok_send);
-				}
+                            es.eucm.eadventure.common.auxiliar.ReportDialog.sendReport(subject + "\n\r" + message + "\n\r" + text);
+                        }
+                        catch( FileNotFoundException e1 ) {
+                            e1.printStackTrace();
+                        }
+                        catch( IOException e1 ) {
+                            e1.printStackTrace();
+                        }
+            			*/
+            			
+            			SendMail sm = new SendMail(assessmentProfile.getSmtpServer(), assessmentProfile.getSmtpUser(), assessmentProfile.getSmtpPwd());
+            			sm.setPort(Integer.parseInt(assessmentProfile.getSmtpPort()));
+            			sm.setRequiersSSL(assessmentProfile.isSmtpSSL());
+            			File reportFile;
+                        try {
+                            reportFile = java.io.File.createTempFile( "ead", ".html" );
+                            generateHTMLReportFile(reportFile.getAbsolutePath( ), -5);
+                            sm.postMailAttachements(to, subject, message, null, reportFile.getAbsolutePath( ));
+                        }
+                        catch( IOException e1 ) {
+                            e1.printStackTrace();
+                            message+=AssessmentEngine.this.getHTMLReportString(  );
+                            sm.postMail(to, subject, message,null );
+                        }
+            			
+            			GUI.getInstance().restoreFrame();
+            			state = STATE_DONE;
+            		}
+            	});
+            	buttonPanel.add(ok_send);
+            	JButton ok_dont_send = new JButton(TC
+            			.get("Report.OKDontSend"));
+            	ok_dont_send.addActionListener(new ActionListener() {
+            		public void actionPerformed(ActionEvent e) {
+            			GUI.getInstance().restoreFrame();
+            			state = STATE_DONE;
+            		}
+            	});
+            	buttonPanel.add(ok_dont_send);
+            	// buttonPanel.getRootPane().setDefaultButton(ok_send);
+            }
 
-				panel.add(buttonPanel, BorderLayout.SOUTH);
+            panel.add(buttonPanel, BorderLayout.SOUTH);
 
-				GUI.getInstance().showComponent(panel);
-
-				// new ReportDialog( GUI.getInstance( ).getFrame( ),
-					// assessmentEngine, adventureName );
-			} catch (MalformedURLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
+            GUI.getInstance().showComponent(panel);
 			return false;
 		} else if (state == STATE_STARTED) {
 		    	GUI.getInstance().componentRepaint();
