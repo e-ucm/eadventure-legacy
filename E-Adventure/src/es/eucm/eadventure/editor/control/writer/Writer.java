baa6c61ce7b4972cdfa1aac1655cdaf0b953ed13
Javier Torrente Vigil <jtorrente84@gmail.com>
2011-04-13 16:26:29 +0000

    Updates for GameTel. Shouldn't affect anything

--- E-Adventure/src/es/eucm/eadventure/editor/control/writer/Writer.java
+++ E-Adventure/src/es/eucm/eadventure/editor/control/writer/Writer.java
@@ -42,6 +42,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.zip.ZipEntry;
@@ -419,6 +420,11 @@ public class Writer {
     }
 
     private static boolean writeWebPage( String tempDir, String loName, boolean windowed, String mainClass ) {
+        return writeWebPage ( tempDir, loName, windowed, mainClass, null);
+    }
+    
+    private static boolean writeWebPage( String tempDir, String loName, boolean windowed, String mainClass, HashMap<String, String> additionalParams ) {
+
 
         boolean dataSaved = true;
         try {
@@ -466,8 +472,18 @@ public class Writer {
             //"\t\t<param name=\"USER_ID\" value=\"567\"/>\n" + "\t\t<param name=\"RUN_ID\" value=\"5540\"/>\n" +
             //The game is initating.. please be patient while the digital sign is verified
  
-            jscript + "\t</head>\n" + "\t<body>\n" + "\t\t<applet code=\"" + mainClass + "\" archive=\"./" + loName + ".jar\" name=\"eadventure\" id=\"eadventure\" " + ( windowed ? "width=\"200\" height=\"150\"" : "width=\"800\" height=\"600\"" ) +  " MAYSCRIPT>\n" + "\t\t<param name=\"WINDOWED\" value=\"" + ( windowed ? "yes" : "no" ) + "\"/>\n" + "\t\t<param name=\"java_arguments\" value=\"-Xms512m -Xmx512m\"/>\n"+ "\t\t<param name=\"image\" value=\"splashScreen.gif\"/>\n"+
-            "\t\t</applet>\n" + "<div id=\"loadingMessage\"><p><b>"+TC.get( "Applet.LoadingMessage" )+"</b></p></div>\n" + "\t</body>\n" + "</html>\n";
+            jscript + "\t</head>\n" + "\t<body>\n" + "\t\t<applet code=\"" + mainClass + "\" archive=\"./" + loName + ".jar\" name=\"eadventure\" id=\"eadventure\" " + ( windowed ? "width=\"200\" height=\"150\"" : "width=\"800\" height=\"600\"" ) +  " MAYSCRIPT>\n" + "\t\t<param name=\"WINDOWED\" value=\"" + ( windowed ? "yes" : "no" ) + "\"/>\n" + "\t\t<param name=\"java_arguments\" value=\"-Xms256m -Xmx512m\"/>\n"+ "\t\t<param name=\"image\" value=\"splashScreen.gif\"/>\n";
+            
+            // Add additional params
+            if (additionalParams!=null){
+                for (String param: additionalParams.keySet( )){
+                    if (param!=null && additionalParams.get( param )!=null){
+                        String value = additionalParams.get( param );
+                        webPage+= "\t\t<param name=\"" +param+"\" value=\""+value+"\"/>\n";
+                    }
+                }
+            }
+            webPage+="\t\t</applet>\n" + "<div id=\"loadingMessage\"><p><b>"+TC.get( "Applet.LoadingMessage" )+"</b></p></div>\n" + "\t</body>\n" + "</html>\n";
 
             File pageFile = new File( tempDir + "/" + loName + ".html" );
             pageFile.createNewFile( );
@@ -1047,6 +1063,138 @@ public class Writer {
 
         return dataSaved;
     }
+
+    
+    public static boolean exportAsGAMETELLearningObject( String zipFilename, String loName, String authorName, String organization, 
+            boolean windowed, String gameFilename, String testReturnURI, String testUserID, AdventureDataControl adventureData) {
+
+        boolean dataSaved = true;
+
+        try {
+            // Create the necessary elements for building the DOM
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance( );
+            TransformerFactory tFactory = TransformerFactory.newInstance( );
+            DocumentBuilder db = dbf.newDocumentBuilder( );
+            Document doc = null;
+            Transformer transformer = null;
+            OutputStream fout = null;
+            OutputStreamWriter writeFile = null;
+
+            //Clean temp directory
+            File tempDir = new File(Controller.createTempDirectory( ).getAbsolutePath( ));
+
+
+            //for(File tempFile:tempDir.listFiles(tempDir.getArchiveDetector( ) )){
+            for( File tempFile : tempDir.listFiles( ) ) {
+                if( tempFile.isDirectory( ) )
+                    tempFile.deleteAll( );
+                tempFile.delete( );
+            }
+
+            // Copy the web to the zip
+            HashMap<String, String> additionalParams = new HashMap<String, String>();
+            if (testReturnURI!=null && testUserID!=null && testReturnURI.length( )>0 && testUserID.length( )>0){
+                additionalParams.put( "return-uri", testReturnURI );
+                additionalParams.put( "user-id", testUserID );
+            }
+            dataSaved &= writeWebPage( tempDir.getAbsolutePath( ), loName, windowed, "es.eucm.eadventure.engine.EAdventureAppletGAMETEL", additionalParams );
+
+            // Merge project & e-Adventure jar into file eAdventure_temp.jar
+            // Destiny file
+            File jarUnsigned = new File( tempDir.getAbsolutePath( ) + "/eAdventure.zip" );
+
+            // Create output stream     
+            FileOutputStream mergedFile = new FileOutputStream( jarUnsigned );
+
+            // Create zipoutput stream
+            ZipOutputStream os = new ZipOutputStream( mergedFile );
+            
+            String manifestText = Writer.defaultManifestFile( "es.eucm.eadventure.engine.EAdventureAppletGAMETEL" );
+            ZipEntry manifestEntry = new ZipEntry( "META-INF/MANIFEST.MF" );
+            ZipEntry manifestEntry2 = new ZipEntry( "META-INF/services/javax.xml.parsers.SAXParserFactory" );
+            ZipEntry manifestEntry3 = new ZipEntry( "META-INF/services/javax.xml.parsers.DocumentBuilderFactory" );
+            os.putNextEntry( manifestEntry );
+            os.write( manifestText.getBytes( ) );
+            os.putNextEntry( manifestEntry2 );
+            os.putNextEntry( manifestEntry3 );
+            os.closeEntry( );
+
+            // Merge projectDirectory and web/eAdventure_temp.jar into output stream
+            File.mergeZipAndDirToJar( "web/eAdventure_temp.jar", gameFilename, os );
+            addNeededLibrariesToJar(os, Controller.getInstance( ));
+
+          
+            os.close( );
+
+            dataSaved &= jarUnsigned.renameTo( new File( tempDir.getAbsolutePath( ) + "/" + loName + "_unsigned.jar" ) );
+
+            // Integrate game and jar into a new jar File
+
+            dataSaved = JARSigner.signJar( authorName, organization, tempDir.getAbsolutePath( ) + "/" + loName + "_unsigned.jar", tempDir.getAbsolutePath( ) + "/" + loName + ".jar" );
+
+            new File( tempDir.getAbsolutePath( ) + "/" + loName + "_unsigned.jar" ).delete( );
+
+            //copy loadingImage
+            File splashScreen = new File( "web/splashScreen.gif" );
+            if ( windowed ){
+                splashScreen = new File( "web/splashScreen_red.gif");
+            }
+            splashScreen.copyTo( new File( tempDir.getAbsolutePath( ) + "/splashScreen.gif" ) );
+            
+            
+            /** ******* START WRITING THE IOParameters ********* */
+       
+                
+            db = dbf.newDocumentBuilder( );
+            
+            doc = db.newDocument( );
+            
+            Element param = ExpectedGameIODOMWriter.buildExpectedInputs( doc, getAdaptationRules(adventureData.getAdventureData( ).getChapters( )), getAssessmemtRules(adventureData.getAdventureData( ).getChapters( )) );
+
+            indentDOM( param, 0 );
+            doc.adoptNode( param );
+            doc.appendChild( param );
+
+            
+            // Create the necessary elements for export the DOM into a XML file
+            transformer = tFactory.newTransformer( );
+
+            // Create the output buffer, write the DOM and close it
+            //fout = new FileOutputStream( zipFilename + "/imsmanifest.xml" );
+            fout = new FileOutputStream(tempDir.getAbsolutePath( ) + "/ead-parameters.xml" );
+            writeFile = new OutputStreamWriter( fout, "UTF-8" );
+            transformer.transform( new DOMSource( doc ), new StreamResult( writeFile ) );
+            writeFile.close( );
+            fout.close( );
+            /** ******* END WRITING THE IOParameters********* */
+
+            /** COPY EVERYTHING TO THE ZIP */
+            File.zipDirectory( tempDir.getAbsolutePath( ) + "/", zipFilename );
+        }
+        catch( IOException exception ) {
+            Controller.getInstance( ).showErrorDialog( TC.get( "Error.Title" ), TC.get( "Error.WriteData" ) );
+            ReportDialog.GenerateErrorReport( exception, true, TC.get( "Error.WriteData" ) );
+            dataSaved = false;
+        }
+        catch( ParserConfigurationException exception ) {
+            Controller.getInstance( ).showErrorDialog( TC.get( "Error.Title" ), TC.get( "Error.WriteData" ) );
+            ReportDialog.GenerateErrorReport( exception, true, TC.get( "Error.WriteData" ) );
+            dataSaved = false;
+        }
+        catch( TransformerConfigurationException exception ) {
+            Controller.getInstance( ).showErrorDialog( TC.get( "Error.Title" ), TC.get( "Error.WriteData" ) );
+            ReportDialog.GenerateErrorReport( exception, true, TC.get( "Error.WriteData" ) );
+            dataSaved = false;
+        }
+        catch( TransformerException exception ) {
+            Controller.getInstance( ).showErrorDialog( TC.get( "Error.Title" ), TC.get( "Error.WriteData" ) );
+            ReportDialog.GenerateErrorReport( exception, true, TC.get( "Error.WriteData" ) );
+            dataSaved = false;
+        }
+
+        return dataSaved;
+    }
+
     
     private static List<AdaptationRule> getAdaptationRules(List<Chapter> cs){
         ArrayList<AdaptationRule> adapt = new ArrayList<AdaptationRule>();
