36558532f6e0c2ccd297a85d8b3d6608ab159d63
Javier Torrente Vigil <jtorrente84@gmail.com>
2012-02-16 10:22:09 +0000

    Headers updated

--- E-Adventure/src/es/eucm/eadventure/engine/gamelog/HighLevelEvents.java
+++ E-Adventure/src/es/eucm/eadventure/engine/gamelog/HighLevelEvents.java
@@ -34,7 +34,6 @@
  *      You should have received a copy of the GNU Lesser General Public License
  *      along with <e-Adventure>.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
-
 package es.eucm.eadventure.engine.gamelog;
 
 
