de6c33eda7e2350db8bf7bc425b7541797dfc61d
Angel Serrano Laguna <angelserranolaguna@gmail.com>
2013-01-15 11:33:46 +0000

    Minor bug fix in GleanerLogConsumer

--- E-Adventure/src-prv/es/eucm/eadventure/tracking/prv/gleaner/GleanerLogConsumer.java
+++ E-Adventure/src-prv/es/eucm/eadventure/tracking/prv/gleaner/GleanerLogConsumer.java
@@ -67,7 +67,7 @@ public class GleanerLogConsumer extends GameLogConsumer {
 
     private static final String START = "start/";
 
-    private static final String TRACK = "track";
+    private static final String TRACK = "track/";
 
     private Gson gson;
 
@@ -141,7 +141,7 @@ public class GleanerLogConsumer extends GameLogConsumer {
             trackPost.setEntity( new StringEntity( jsonTraces ) );
             HttpResponse response = client.execute( trackPost );
             int statusCode = response.getStatusLine( ).getStatusCode( );
-            return statusCode == 200;
+            return statusCode == 204;
         }
         catch( UnsupportedEncodingException e ) {
             return false;
@@ -157,9 +157,8 @@ public class GleanerLogConsumer extends GameLogConsumer {
     @Override
     protected boolean consumerClose( List<GameLogEntry> newQ ) {
 
-        
         consumerCode( newQ );
-        
+
         if( connected ) {
             Map<String, Object> gameQuitTrace = new HashMap<String, Object>( );
             gameQuitTrace.put( "timeStamp", System.currentTimeMillis( ) );
@@ -167,7 +166,7 @@ public class GleanerLogConsumer extends GameLogConsumer {
             gameQuitTrace.put( "event", "game_quit" );
             traces.add( gameQuitTrace );
         }
-        
+
         connected = false;
         return false;
     }
@@ -214,186 +213,193 @@ public class GleanerLogConsumer extends GameLogConsumer {
         LinkedHashMap<String, Object> trace = new LinkedHashMap<String, Object>( );
         LinkedHashMap<String, Object> data = new LinkedHashMap<String, Object>( );
 
-        // High level
-        if( entry.getElementName( ).equals( "h" ) ) {
-            trace.put( "timeStamp", Long.parseLong( entry.getAttributeValue( "ms" ) ) + initTimeStamp );
-            trace.put( "type", "logic" );
+        try {
+            // High level
+            if( entry.getElementName( ).equals( "h" ) ) {
+                trace.put( "timeStamp", Long.parseLong( entry.getAttributeValue( "ms" ) ) + initTimeStamp );
+                trace.put( "type", "logic" );
 
-            if( entry.getAttributeValue( "o" ) != null ) {
-                trace.put( "target", entry.getAttributeValue( "o" ) );
-            }
+                if( entry.getAttributeValue( "o" ) != null ) {
+                    trace.put( "target", entry.getAttributeValue( "o" ) );
+                }
 
-            if( entry.getAttributeValue( "t" ) != null ) {
-                if( trace.containsKey( "t" ) ) {
-                    data.put( "target", entry.getAttributeValue( "t" ) );
+                if( entry.getAttributeValue( "t" ) != null ) {
+                    if( trace.containsKey( "t" ) ) {
+                        data.put( "target", entry.getAttributeValue( "t" ) );
+                    }
+                    else {
+                        trace.put( "target", entry.getAttributeValue( "t" ) );
+                    }
                 }
-                else {
-                    trace.put( "target", entry.getAttributeValue( "t" ) );
-                }
-            }
 
-            if( "scn".equals( entry.getAttributeValue( "a" ) ) ) {
-                if( currentPhase != null ) {
-                    // Send phase_end trace
-                    LinkedHashMap<String, Object> endPhaseTrace = new LinkedHashMap<String, Object>( );
-                    endPhaseTrace.put( "type", "logic" );
-                    endPhaseTrace.put( "timeStamp", (Long) trace.get( "timeStamp" ) - 1);
-                    endPhaseTrace.put( "event", "phase_end" );
-                    endPhaseTrace.put( "target", currentPhase );
-                    traces.add( endPhaseTrace );                
+                if( "scn".equals( entry.getAttributeValue( "a" ) ) ) {
+                    if( currentPhase != null ) {
+                        // Send phase_end trace
+                        LinkedHashMap<String, Object> endPhaseTrace = new LinkedHashMap<String, Object>( );
+                        endPhaseTrace.put( "type", "logic" );
+                        endPhaseTrace.put( "timeStamp", (Long) trace.get( "timeStamp" ) - 1 );
+                        endPhaseTrace.put( "event", "phase_end" );
+                        endPhaseTrace.put( "target", currentPhase );
+                        traces.add( endPhaseTrace );
+                    }
+                    // Set phase_start trace
+                    trace.put( "event", "phase_start" );
+                    this.currentPhase = trace.get( "target" ).toString( );
                 }
-                // Set phase_start trace
-                trace.put( "event", "phase_start" );
-                this.currentPhase = trace.get( "target" ).toString( );
-            }
-            else if( entry.getAttributeValue( "a" ) != null ) {
-                trace.put( "event", entry.getAttributeValue( "a" ) );
-            }
-            else if( entry.getAttributeValue( "e" ) != null ) {
-                // Activate / Deactive flag
-                String attValue = entry.getAttributeValue( "e" );
-                trace.put( "event", attValue );
-                String varName = entry.getAttributeValue( "t" );
-                trace.put( "target", varName );
-                boolean varUpdate = false;
-                boolean value = false;
-                if( attValue.equals( "act" ) ) {
+                else if( entry.getAttributeValue( "a" ) != null ) {
+                    trace.put( "event", entry.getAttributeValue( "a" ) );
+                }
+                else if( entry.getAttributeValue( "e" ) != null ) {
+                    // Activate / Deactive flag
+                    String attValue = entry.getAttributeValue( "e" );
+                    trace.put( "event", attValue );
+                    String varName = entry.getAttributeValue( "t" );
+                    trace.put( "target", varName );
+                    boolean varUpdate = false;
+                    boolean value = false;
+                    if( attValue.equals( "act" ) ) {
+                        trace.put( "event", "var_update" );
+                        varUpdate = true;
+                        value = true;
+                    }
+                    else if( attValue.equals( "dct" ) ) {
+                        trace.put( "event", "var_update" );
+                        varUpdate = true;
+                        value = false;
+                    }
+
+                    if( varUpdate ) {
+                        data.put( "value", value );
+                        data.put( "operator", attValue );
+                    }
+                }
+
+                // Change var value
+                if( entry.getAttributeValue( "v" ) != null ) {
+                    Integer value = Integer.parseInt( entry.getAttributeValue( "v" ) );
+                    data.put( "operand", value );
+                    String operator = entry.getAttributeValue( "e" );
+                    data.put( "operator", operator );
                     trace.put( "event", "var_update" );
-                    varUpdate = true;
-                    value = true;
-                }
-                else if( attValue.equals( "dct" ) ) {
-                    trace.put( "event", "var_update" );
-                    varUpdate = true;
-                    value = false;
+                    String varName = entry.getAttributeValue( "t" );
+                    trace.put( "target", varName );
+
+                    if( !vars.containsKey( trace.get( "target" ) ) ) {
+                        vars.put( varName, 0 );
+                    }
+
+                    if( operator.equals( "set" ) ) {
+                        vars.put( varName, value );
+                    }
+                    else if( operator.equals( "inc" ) ) {
+                        vars.put( varName, value + vars.get( varName ) );
+                    }
+                    else if( operator.equals( "dec" ) ) {
+                        vars.put( varName, value - vars.get( varName ) );
+                    }
+
+                    data.put( "value", vars.get( varName ) );
                 }
 
-                if( varUpdate ) {
-                    data.put( "value", value );
-                    data.put( "operator", attValue );
+                // Other data
+                if( entry.getAttributeValue( "ix" ) != null ) {
+                    data.put( "ix", Integer.parseInt( entry.getAttributeValue( "ix" ) ) );
+                }
+                if( entry.getAttributeValue( "iy" ) != null ) {
+                    data.put( "iy", Integer.parseInt( entry.getAttributeValue( "iy" ) ) );
+                }
+                if( entry.getAttributeValue( "x" ) != null ) {
+                    data.put( "x", Integer.parseInt( entry.getAttributeValue( "x" ) ) );
+                }
+                if( entry.getAttributeValue( "y" ) != null ) {
+                    data.put( "y", Integer.parseInt( entry.getAttributeValue( "y" ) ) );
+                }
+                if( entry.getAttributeValue( "dx" ) != null ) {
+                    data.put( "dx", Integer.parseInt( entry.getAttributeValue( "dx" ) ) );
+                }
+                if( entry.getAttributeValue( "dy" ) != null ) {
+                    data.put( "dy", Integer.parseInt( entry.getAttributeValue( "dy" ) ) );
+                }
+                if( entry.getAttributeValue( "l" ) != null ) {
+                    data.put( "l", entry.getAttributeValue( "l" ) );
+                }
+            }
+            // Low level
+            else if( entry.getElementName( ).equals( "l" ) ) {
+                trace.put( "timeStamp", Long.parseLong( entry.getAttributeValue( "ms" ) ) + initTimeStamp );
+                trace.put( "type", "input" );
+                String type = entry.getAttributeValue( "m" );
+                String action = entry.getAttributeValue( "i" );
+                if( "m".equals( type ) ) {
+                    trace.put( "device", "mouse" );
+                    data.put( "count", Integer.parseInt( entry.getAttributeValue( "c" ) ) );
+                    data.put( "button", Integer.parseInt( entry.getAttributeValue( "b" ) ) );
+                    String offset = entry.getAttributeValue( "off" );
+                    if( offset != null ) {
+                        data.put( "offset", Integer.parseInt( offset ) );
+                    }
+                }
+                else if( "k".equals( type ) ) {
+                    trace.put( "device", "keyboard" );
+                    if( "t".equals( action ) ) {
+                        data.put( "ch", entry.getAttributeValue( "k" ) );
+                    }
+                    else {
+                        data.put( "keyCode", Integer.parseInt( entry.getAttributeValue( "c" ) ) );
+                    }
+                }
+
+                // Action
+                if( "m".equals( action ) ) {
+                    trace.put( "action", "move" );
+                }
+                else if( "p".equals( action ) ) {
+                    trace.put( "action", "press" );
+                }
+                else if( "r".equals( action ) ) {
+                    trace.put( "action", "release" );
+                }
+                else if( "c".equals( action ) ) {
+                    trace.put( "action", "click" );
+                }
+                else if( "en".equals( action ) ) {
+                    trace.put( "action", "enter" );
+                }
+                else if( "d".equals( action ) ) {
+                    trace.put( "action", "drag" );
+                }
+                else if( "ex".equals( action ) ) {
+                    trace.put( "action", "exit" );
+                }
+                else if( "t".equals( action ) ) {
+                    trace.put( "action", "type" );
+                }
+
+                if( entry.getAttributeValue( "m" ) != null ) {
+                    data.put( "modifiers", entry.getAttributeValue( "m" ) );
+                }
+
+                if( entry.getAttributeValue( "x" ) != null ) {
+                    data.put( "x", Integer.parseInt( entry.getAttributeValue( "x" ) ) );
+                }
+
+                if( entry.getAttributeValue( "y" ) != null ) {
+                    data.put( "y", Integer.parseInt( entry.getAttributeValue( "y" ) ) );
                 }
             }
 
-            // Change var value
-            if( entry.getAttributeValue( "v" ) != null ) {
-                Integer value = Integer.parseInt( entry.getAttributeValue( "v" ) );
-                data.put( "operand", value );
-                String operator = entry.getAttributeValue( "e" );
-                data.put( "operator", operator );
-                trace.put( "event", "var_update" );
-                String varName = entry.getAttributeValue( "t" );
-                trace.put( "target", varName );
-
-                if( !vars.containsKey( trace.get( "target" ) ) ) {
-                    vars.put( varName, 0 );
-                }
-
-                if( operator.equals( "set" ) ) {
-                    vars.put( varName, value );
-                }
-                else if( operator.equals( "inc" ) ) {
-                    vars.put( varName, value + vars.get( varName ) );
-                }
-                else if( operator.equals( "dec" ) ) {
-                    vars.put( varName, value - vars.get( varName ) );
-                }
-
-                data.put( "value", vars.get( varName ) );
+            if( trace.isEmpty( ) ) {
+                return null;
             }
 
-            // Other data
-            if( entry.getAttributeValue( "ix" ) != null ) {
-                data.put( "ix", Integer.parseInt( entry.getAttributeValue( "ix" ) ) );
+            if( !data.isEmpty( ) ) {
+                trace.put( "data", data );
             }
-            if( entry.getAttributeValue( "iy" ) != null ) {
-                data.put( "iy", Integer.parseInt( entry.getAttributeValue( "iy" ) ) );
-            }
-            if( entry.getAttributeValue( "x" ) != null ) {
-                data.put( "x", Integer.parseInt( entry.getAttributeValue( "x" ) ) );
-            }
-            if( entry.getAttributeValue( "y" ) != null ) {
-                data.put( "y", Integer.parseInt( entry.getAttributeValue( "y" ) ) );
-            }
-            if( entry.getAttributeValue( "dx" ) != null ) {
-                data.put( "dx", Integer.parseInt( entry.getAttributeValue( "dx" ) ) );
-            }
-            if( entry.getAttributeValue( "dy" ) != null ) {
-                data.put( "dy", Integer.parseInt( entry.getAttributeValue( "dy" ) ) );
-            }
-            if( entry.getAttributeValue( "l" ) != null ) {
-                data.put( "l", entry.getAttributeValue( "l" ) );
-            }
+
         }
-        // Low level
-        else if( entry.getElementName( ).equals( "l" ) ) {
-            trace.put( "timeStamp", Long.parseLong( entry.getAttributeValue( "ms" ) ) + initTimeStamp );
-            trace.put( "type", "input" );
-            String type = entry.getAttributeValue( "m" );
-            String action = entry.getAttributeValue( "i" );
-            if( "m".equals( type ) ) {
-                trace.put( "device", "mouse" );
-                data.put( "count", Integer.parseInt( entry.getAttributeValue( "c" ) ) );
-                data.put( "button", Integer.parseInt( entry.getAttributeValue( "b" ) ) );
-                String offset = entry.getAttributeValue( "off" );
-                if( offset != null ) {
-                    data.put( "offset", Integer.parseInt( offset ) );
-                }
-            }
-            else if( "k".equals( type ) ) {
-                trace.put( "device", "keyboard" );
-                if( "t".equals( action ) ) {
-                    data.put( "ch", entry.getAttributeValue( "k" ) );
-                }
-                else {
-                    data.put( "keyCode", Integer.parseInt( entry.getAttributeValue( "c" ) ) );
-                }
-            }
-
-            // Action
-            if( "m".equals( action ) ) {
-                trace.put( "action", "move" );
-            }
-            else if( "p".equals( action ) ) {
-                trace.put( "action", "press" );
-            }
-            else if( "r".equals( action ) ) {
-                trace.put( "action", "release" );
-            }
-            else if( "c".equals( action ) ) {
-                trace.put( "action", "click" );
-            }
-            else if( "en".equals( action ) ) {
-                trace.put( "action", "enter" );
-            }
-            else if( "d".equals( action ) ) {
-                trace.put( "action", "drag" );
-            }
-            else if( "ex".equals( action ) ) {
-                trace.put( "action", "exit" );
-            }
-            else if( "t".equals( action ) ) {
-                trace.put( "action", "type" );
-            }
-
-            if( entry.getAttributeValue( "m" ) != null ) {
-                data.put( "modifiers", entry.getAttributeValue( "m" ) );
-            }
-
-            if( entry.getAttributeValue( "x" ) != null ) {
-                data.put( "x", Integer.parseInt( entry.getAttributeValue( "x" ) ) );
-            }
-
-            if( entry.getAttributeValue( "y" ) != null ) {
-                data.put( "y", Integer.parseInt( entry.getAttributeValue( "y" ) ) );
-            }
-        }
-
-        if( trace.isEmpty( ) ) {
-            return null;
-        }
-
-        if( !data.isEmpty( ) ) {
-            trace.put( "data", data );
+        catch( Exception e ) {
+            System.out.println("Exception while converting traces: " + e);
+            System.out.println( entry + "");
         }
         return trace;
 
